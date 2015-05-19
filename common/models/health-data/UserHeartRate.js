8699508ddc513b465613f04ca4b2372a413a995e
Ninh Pham <ninhpham@hotmail.com>
2015-05-19 18:39:20 +0930

    Integrate with Dicky and Yugu

---
+++ common/models/health-data/UserHeartRate.js
@@ -0,0 +1,54 @@
+var loopback = require('loopback');
+var heartrate = require(__dirname + '/../../../lib/analysis/heartrate.js');
+
+module.exports = function(model) {
+	execute = function(userId, startDate, endDate, values, cb) {
+		var ctx = loopback.getCurrentContext();
+		var app = ctx && ctx.get('app');
+		app.models.UserHeartRate.find({where: { UserId: userId, RecordTime: {between: [startDate+' 00:00:00', endDate+' 23:59:59']}}}, function(err, results){
+			console.log(results);
+			var avg = heartrate.CalHeartRate(results, values);
+			cb(null, results, avg);
+			
+		});
+	};
+
+	model.current = function(id, start, end, cb) {
+		execute(id, start, end, 'current', cb);
+	};
+	model.average = function(id, start, end, cb) {
+		execute(id, start, end, 'average', cb);
+	};
+	model.remoteMethod(
+        'current', 
+        {
+        	accepts: [
+        		{arg: 'id', type: 'int'},
+        		{arg: 'start', type: 'string'},
+        		{arg: 'end', type: 'string'}
+        	],
+        	returns: [
+        		{arg: 'data' },
+        		{arg: 'avg', type: 'double'}
+        	],
+        	http: {path: '/current', verb: 'get'},
+        	description: "Get the current heart rate per second"
+        }
+    );
+    model.remoteMethod(
+        'average', 
+        {
+        	accepts: [
+        		{arg: 'id', type: 'int'},
+        		{arg: 'start', type: 'string'},
+        		{arg: 'end', type: 'string'}
+        	],
+        	returns: [
+        		{arg: 'data' },
+        		{arg: 'avg', type: 'double'}
+        	],
+        	http: {path: '/average', verb: 'get'},
+        	description: "Get the average heart rate per second"
+        }
+    );
+};
---
+++ common/models/health-data/Walking.js
@@ -0,0 +1,6 @@
+var loopback = require('loopback');
+var heartrate = require(__dirname + '/../../../lib/analysis/heartrate.js');
+
+module.exports = function(model) {
+
+};
