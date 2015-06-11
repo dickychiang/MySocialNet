0ec6297d2bbe8b7724698bd09be0b574a155c13d
Ninh Pham <ninhpham@hotmail.com>
2015-06-11 16:55:17 +0930

    Video with PeerJS and Avatar

--- server/server.js
+++ server/server.js
@@ -77,44 +77,12 @@ if (require.main === module) {
 			app.io.emit('online');
 			socket.join('all');
 		});
-		
-		socket.on('message', function(message) {
-			socket.broadcast.emit('message', message);
-		});
-		
-		socket.on('chat', function(message) {
-			socket.broadcast.emit('chat', message);
-		});
-		
-		socket.on('create or join', function(room) {
-			console.log(app.io.sockets);
-			var numClients = app.io.sockets.clients(room).length;
-			if (numClients === 0) {
-				socket.join(room);
-				socket.emit('created', room);
-			} else if (numClients == 1) {
-				app.io.sockets. in (room).emit('join', room);
-				socket.join(room);
-				socket.emit('joined', room);
-			} else {
-				socket.emit('full', room);
-			}
-			socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-			socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
-		});
 
 		socket.on('call', function(offer) {
-			// from: userId
-			// to: userId
-			if (this.accessToken && this.accessToken.userId == offer.from) {
-				for (var key in app.io.connected) {
-					var conn = app.io.connected[key];
-					if (conn.accessToken && offer.to == conn.accessToken.userId) {
-						console.log(conn);
-						break;
-					}
-				}
-			}
+			// fromUserId: {userId}
+			// toUserId: {userId}
+			// fromPeerId: string
+			app.io.emit('call', offer);
 		});
 
 		socket.on('disconnect', function(){
